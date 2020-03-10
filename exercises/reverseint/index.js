c62a3e2db5de27d5792cab886807ff27101890bc
Alex Perkowski <perkowskialex@gmail.com>
2020-03-10 15:22:18 -0500

    reverse int solve

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,17 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  // find negative or positive
+  let sign = Math.sign(n);
+  // to string
+  n = n
+    .toString()
+    .split("")
+    .reverse()
+    .join("");
+  // make sure its negative or positive
+  return sign * parseInt(n);
+}
 
 module.exports = reverseInt;
