09b88157c162b48a8b3fe160dfd595753c717b31
Alex Perkowski <perkowskialex@gmail.com>
2020-03-10 17:38:47 -0500

    solve capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -6,7 +6,17 @@
 //   capitalize('a short sentence') --> 'A Short Sentence'
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
-
-function capitalize(str) {}
+function capitalize(str) {
+  let words = [];
+  // split string
+  str = str.split(" ");
+  // for loop
+  // upper case 1st letter
+  // join w string
+  // push result into words array
+  for (let i of str) words.push(i[0].toUpperCase() + i.slice(1));
+  // join words into a string and return
+  return words.join(" ");
+}
 
 module.exports = capitalize;
