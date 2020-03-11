59940099b97a8f0c318911961e47e3ea229da1e2
Alex Perkowski <perkowskialex@gmail.com>
2020-03-11 15:17:09 -0500

    pyramid solve , kinda ugly tho

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,27 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  // create an empty string / set vars
+  let space = " ";
+  let hash = "#";
+  let step = "";
+  let count = 1;
+  if (n === 1) {
+    return hash;
+  } else
+    while (count <= n) {
+      // spaces on the sides are equal to n-count
+      // number of steps outside the middle column is 2 more than count, so plus 1 on each side
+      step = space
+        .repeat(n - count)
+        .concat("", hash.repeat(count * 2 - 1))
+        .concat("", space.repeat(n - count));
+      count++;
+      console.log(step);
+      space = " ";
+      step = "";
+    }
+}
 
 module.exports = pyramid;
