fe7eb4f443aff5227f693758c11b7e19fa6b6f18
Alex Perkowski <perkowskialex@gmail.com>
2020-03-11 15:41:42 -0500

    find vowels complete

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,21 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
-
+function vowels(str) {
+  // empty array for holding vowels
+  let vowels = ["a", "e", "i", "o", "u"];
+  let arr = [];
+  // place str to lower case and split for iteration
+  str = str.toLowerCase().split("");
+  // loop
+  for (let i = 0; i < str.length; i++) {
+    // if vowel not in array, push
+    if (vowels.includes(str[i])) {
+      arr.push(str[i]);
+      console.log(str[i]);
+    }
+  }
+  return arr.length;
+}
+// return length of array
 module.exports = vowels;
