95847be8ad3d7dc17c3e2eb90c309ab54b5982ef
Alex Perkowski <perkowskialex@gmail.com>
2020-03-11 14:35:51 -0500

    solve steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -16,7 +16,22 @@
 //       '##  '
 //       '### '
 //       '####'
-
-function steps(n) {}
-
+function steps(n) {
+  // create an empty string / set vars
+  let space = " ";
+  let hash = "#";
+  let step = "";
+  let count = 1;
+  // while loop that checks if n is a certain int value, then
+  while (n >= count) {
+    // make step using hash
+    step = step.concat("", hash.repeat(count));
+    step = step.concat(space.repeat(n - count));
+    // console log the step
+    console.log(step);
+    // increment count
+    count++;
+    step = "";
+  }
+}
 module.exports = steps;
