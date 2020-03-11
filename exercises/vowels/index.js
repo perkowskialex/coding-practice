e3c5642c3a38a2354aca1ab150d073324271e6b1
Alex Perkowski <perkowskialex@gmail.com>
2020-03-11 15:57:43 -0500

    match method for vowel match

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,21 +7,28 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
+// function vowels(str) {
+//   // empty array for holding vowels
+//   let vowels = ["a", "e", "i", "o", "u"];
+//   let arr = [];
+//   // place str to lower case and split for iteration
+//   str = str.toLowerCase().split("");
+//   // loop
+//   for (let i = 0; i < str.length; i++) {
+//     // if vowel not in array, push
+//     if (vowels.includes(str[i])) {
+//       arr.push(str[i]);
+//       console.log(str[i]);
+//     }
+//   }
+//   return arr.length;
+// }
+
+// second method
 function vowels(str) {
-  // empty array for holding vowels
-  let vowels = ["a", "e", "i", "o", "u"];
-  let arr = [];
-  // place str to lower case and split for iteration
-  str = str.toLowerCase().split("");
-  // loop
-  for (let i = 0; i < str.length; i++) {
-    // if vowel not in array, push
-    if (vowels.includes(str[i])) {
-      arr.push(str[i]);
-      console.log(str[i]);
-    }
-  }
-  return arr.length;
+  let matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
 }
+
 // return length of array
 module.exports = vowels;
