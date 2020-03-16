a4897a69143923b9086b23bc004d54f6556b7806
Alex Perkowski <perkowskialex@gmail.com>
2020-03-16 14:38:21 -0500

    solve fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,23 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// 0  1  1
+// a  b  c
+
+function fib(n) {
+  let a = 0;
+  let b = 1;
+  let c = a + b;
+  // console.log('start for loop')
+  for (let i = 0; i < n - 2; i++) {
+    a = b;
+    //   console.log(a)
+    b = c;
+    //   console.log(b)
+    c = a + b;
+    //   console.log(c)
+  }
+  return c;
+}
 
 module.exports = fib;
