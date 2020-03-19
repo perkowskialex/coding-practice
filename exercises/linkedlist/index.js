1bd8cbb3402b4eed5875809bd123bedba5adf4c5
Alex Perkowski <perkowskialex@gmail.com>
2020-03-19 14:22:43 -0500

    solve getat for linked list

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -74,6 +74,17 @@ class LinkedList {
       this.head = new Node(data);
     }
   }
+
+  getAt(number) {
+    if (!this.head) return null;
+    let counter = 0;
+    let node = this.head;
+    while (number !== counter) {
+      counter++;
+      node = node.next;
+    }
+    return node;
+  }
 }
 
 module.exports = { Node, LinkedList };
